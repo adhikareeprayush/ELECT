64c635be4ca72a78f454cb11d93de10bfdbb1cc9
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2024-04-12 11:42:19 +0545

    made some changes

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,8 +1,14 @@
 <x-layout>
     <section class=" bg-gray-100 dark:bg-gray-800 ">
         <div class="container mx-auto px-4 pt-3">
-            <h2 class="text-3xl font-semibold text-gray-900 dark:text-white mb-6 pt-3">What We Offer</h2>
-            <div class="grid place-items-center grid-cols-3 gap-6">
+            <div class="flex align-items-center">
+                <h2 class="text-3xl font-semibold text-gray-900 dark:text-white mb-6 pt-3">What We Offer</h2>
+                <button id="searchButton"
+                    class="ml-auto h-fit bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">
+                    Search
+                </button>
+            </div>
+            <div class="grid place-items-start grid-cols-3 gap-6">
                 <!-- Phone Cards -->
 
                 @foreach ($products as $product)
@@ -19,7 +25,7 @@
 
     <!-- Mega Search Box -->
     <div id="megaSearchBoxContainer"
-        class="fixed top-0 left-0 w-screen h-screen bg-gray-800 bg-opacity-75 flex items-center justify-center hidden">
+        class="fixed top-0 left-0 w-screen  h-screen flex bg-gray-800 bg-opacity-75 hidden items-center justify-center">
         <div id="megaSearchBox" class="bg-white rounded-lg shadow-lg w-full max-w-2xl p-6">
             <form class="flex">
                 <input type="text" name="search" class="w-full p-2 rounded-lg border-gray-300 focus:border-blue-500"
