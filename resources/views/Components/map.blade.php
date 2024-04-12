21a22d07cffdeafc8f07687deb82ed21cdd31957
Aayush Adhikari <adhikariaayush37@gmail.com>
2024-04-12 10:43:38 +0545

    changes on map

--- resources/views/Components/map.blade.php
+++ resources/views/Components/map.blade.php
@@ -1,15 +1,13 @@
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-    crossorigin="" />
-<style>
-    #map {
-        height: 400px;
-        /* Adjust as needed */
-        border-radius: 20px;
-        width: 80%;
-    }
-</style>
-
+        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+        crossorigin="" />
+    <style>
+        #map {
+            height: 400px;
+            border-radius: 20px;
+            width: 80%;
+        }
+    </style>
 
 <div id="map"></div>
 
@@ -19,16 +17,29 @@
     crossorigin=""></script>
 
 <script>
-    // Initialize the map
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    // Wait for the DOM to be fully loaded before initializing the map
+    document.addEventListener('DOMContentLoaded', function () {
+        // Initialize the map
+        var map = L.map('map').setView([27.6949663991989, 85.37251786596472], 13);
 
-    // Add the OpenStreetMap tile layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        // Add the OpenStreetMap tile layer
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    // Add a marker at a specific location
-    L.marker([27.6949663991989, 85.37251786596472]).addTo(map)
-        .bindPopup('One Forty')
-        .openPopup();
+        // Define a marker with a popup
+        var marker = L.marker([27.6949663991989, 85.37251786596472])
+            .addTo(map)
+            .bindPopup('One Forty')
+            .openPopup();
+
+        // Add a click event listener to the marker to display coordinates and details
+        marker.on('click', function (e) {
+            var coordinates = e.latlng.lat.toFixed(6) + ', ' + e.latlng.lng.toFixed(6);
+            marker.bindPopup('<b>Coordinates:</b> ' + coordinates + '<br> <b>Place:</b> One Forty').openPopup();
+        });
+
+        // Fit the bounds to the marker
+        map.fitBounds(marker.getBounds());
+    });
 </script>
