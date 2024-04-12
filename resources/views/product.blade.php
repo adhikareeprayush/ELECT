571316feea69260a9e38b8de6cc3a4cea1fb926a
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2024-04-12 20:24:03 +0545

    minute changes

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,9 +4,7 @@
     <div role="status"
         class="space-y-8 justify-center p-4 md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
         <div class="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
-            <img class=""
-                src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Laptop Details">
+            <img class="" src="{{ $product->image }}" alt="Laptop Details">
         </div>
         <div class="w-1/2">
             {{-- Product Details --}}
